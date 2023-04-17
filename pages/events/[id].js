85012b210c0a721bb2c7ad2b5dd8cd180f81aaf7
ShankS3 <shashank.surwase@gmail.com>
2023-04-17 23:44:25 +0530

    Added events detail page

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { useRouter } from 'next/router';
+
+import { getEventById } from '../../dummy-data';
+import EventSummary from '../../components/eventDetail/EventSummary';
+import EventContent from '../../components/eventDetail/EventContent';
+import EventLogistics from '../../components/eventDetail/EventLogistics';
 
 function EventDetailsPage() {
+  const router = useRouter();
+  const { id } = router.query;
+  const event = getEventById(id);
+
+  if(!event) {
+    return <p>No event found !</p>
+  }
+
   return (
-    <div>
-      <h1>Event Details for each event</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   )
 }
 
