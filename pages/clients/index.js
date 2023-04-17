3081bf9aed432aa1238e916b45151ad2b3706777
ShankS3 <shashank.surwase@gmail.com>
2023-04-17 16:30:38 +0530

    Added dynamic routes for navigation

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,6 +1,21 @@
+import Link from "next/link";
+
 function ClientsPage() {
+  const clients = [
+    {id: 'max', name: 'Maximilian'},
+    {id: 'manuel', name: 'Manuel'}
+  ]
   return (
-    <h1>Clients Page</h1>
+    <div>
+      <h1>Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
