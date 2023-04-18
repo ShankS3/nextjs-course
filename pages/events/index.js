b72d889ac55904c17ee8022c9774a55af432aa94
ShankS3 <shashank.surwase@gmail.com>
2023-04-18 11:18:25 +0530

    Added data to all events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
+import EventList from '../../components/event/EventList';
+import { getAllEvents } from '../../dummy-data';
+
 function EventsPage() {
+  const allEvents = getAllEvents();
   return (
     <div>
       <h1>All Events</h1>
+      <EventList list={allEvents} />
     </div>
   )
 }
