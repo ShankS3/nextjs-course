837e2dce6ca20416cf8c3d7c7ca2660c074fddea
ShankS3 <shashank.surwase@gmail.com>
2023-04-18 11:34:23 +0530

    Added events seach form

--- components/common/Button.js
+++ components/common/Button.js
@@ -2,12 +2,16 @@ import Link from 'next/link';
 import React from 'react'
 import classes from './Button.module.css';
 
-function Button({children, link}) {
-  return (
-    <Link href={link} className={classes.btn}>
-      {children}
-    </Link>
-  );
+function Button({children, link, onClick}) {
+  if(link) {
+    return (
+      <Link href={link} className={classes.btn}>
+        {children}
+      </Link>
+    );
+  }
+
+  return <button className={classes.btn} onClick={onClick}>{children}</button>
 }
 
 export default Button;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import EventList from '../../components/event/EventList';
+import EventSearch from '../../components/event/EventSearch';
 import { getAllEvents } from '../../dummy-data';
 
 function EventsPage() {
   const allEvents = getAllEvents();
   return (
-    <div>
+    <Fragment>
       <h1>All Events</h1>
+      <EventSearch />
       <EventList list={allEvents} />
-    </div>
+    </Fragment>
   )
 }
 
